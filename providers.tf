c6a4810b188f872f9e2351a3230b15f954dd25e6
J <me@myhouse.com>
2021-04-24 04:33:00 -0400

    gp2 workaround

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 0.15.0"
 
   required_providers {
     aws        = ">= 3.22.0"
     local      = ">= 1.4"
     null       = ">= 2.1"
-    //template   = ">= 2.1"
+    template   = ">= 2.1"
     random     = ">= 2.1"
     kubernetes = ">= 1.11"
   }
